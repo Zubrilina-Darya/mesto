bf30a14d708f14293eaef6f6a8d8ab6fd66f372c
Zubrilina Darya <Dashaboss2411@yandex.ru>
2023-02-05 09:36:39 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,75 @@
 const openPopup = document.querySelector(".profile__article-edit-button");
-const closePopup = document.querySelector(".popup__close");
 const popup = document.querySelector(".popup");
-let formElement = document.querySelector(".popup__form");
+const formElement = document.querySelector(".popup__form_type_profile");
 const nameInput = document.querySelector(".popup__input_value_name");
 const jobInput = document.querySelector(".popup__input_value_job");
 const profileArticle = document.querySelector(".profile__article-title");
 const job = document.querySelector(".profile__article-text");
+const addButton = document.querySelector(".profile__add-button");
 
-function ClosePopup() {
+
+//попапы
+const popupProfile = document.querySelector(".popup_type_profile");
+const popupItem = document.querySelector(".popup_type_item");
+const closePopupProfileButton =popupProfile.querySelector(".popup__close");
+const closePopupItemButton = popupItem.querySelector(".popup__close");
+
+function ClosePopup(popup) {
   popup.classList.remove("popup_opened");
 }
-function OpenPopup() {
+function OpenPopup(popup) {
   popup.classList.add("popup_opened");
   nameInput.value = profileArticle.textContent;
   jobInput.value = job.textContent;
 }
+
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   // Получите значение полей jobInput и nameInput из свойства value
   profileArticle.textContent = nameInput.value; // Выберите элементы, куда должны быть вставлены значения полей
   job.textContent = jobInput.value; // Вставьте новые значения с помощью textContent
-  ClosePopup();
+  ClosePopup(popup);
 }
 formElement.addEventListener("submit", handleFormSubmit);
 
-openPopup.addEventListener("click", OpenPopup);
-closePopup.addEventListener("click", ClosePopup);
+openPopup.addEventListener("click",() => {
+  OpenPopup(popupProfile);
+}); 
+closePopupProfileButton.addEventListener("click",() => {
+  ClosePopup(popupProfile);
+}); 
+addButton.addEventListener("click", () => {
+  OpenPopup(popupItem);
+});
+closePopupItemButton.addEventListener("click",() => {
+  ClosePopup(popupItem);
+}); 
+
+
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
