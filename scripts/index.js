fa396a53c407d7912f0a3e2895534070501fed02
Zubrilina Darya <Dashaboss2411@yandex.ru>
2023-02-11 13:58:39 +0300

    попытка 1

--- scripts/index.js
+++ scripts/index.js
@@ -7,13 +7,20 @@ const profileArticle = document.querySelector(".profile__article-title");
 const job = document.querySelector(".profile__article-text");
 const addButton = document.querySelector(".profile__add-button");
 
-
 //попапы
 const popupProfile = document.querySelector(".popup_type_profile");
-const popupItem = document.querySelector(".popup_type_item");
-const popupZoom = document.querySelector("popup_type_zoom");
+const popupNewPlace = document.querySelector(".popup_type_item");
+const popupZoom = document.querySelector(".popup_type_zoom");
 const closePopupProfileButton = popupProfile.querySelector(".popup__close");
-const closePopupItemButton = popupItem.querySelector(".popup__close");
+const closePopupNewPlaceButton = popupNewPlace.querySelector(".popup__close");
+const closePopupZoom = popupZoom.querySelector(".popup__close");
+
+const inputPlaceName = popupNewPlace.querySelector(".popup__input_value_title");
+const inputPlaceLink = popupNewPlace.querySelector(".popup__input_value_link");
+const popupNewPlaceSaveButton = popupNewPlace.querySelector(
+  ".popup__button-save"
+);
+const popupNewPlaceForm = popupNewPlace.querySelector(".popup__form_type_item");
 
 function ClosePopup(popup) {
   popup.classList.remove("popup_opened");
@@ -40,13 +47,16 @@ closePopupProfileButton.addEventListener("click", () => {
   ClosePopup(popupProfile);
 });
 addButton.addEventListener("click", () => {
-  OpenPopup(popupItem);
+  OpenPopup(popupNewPlace);
 });
-closePopupItemButton.addEventListener("click", () => {
-  ClosePopup(popupItem);
+closePopupNewPlaceButton.addEventListener("click", () => {
+  ClosePopup(popupNewPlace);
+});
+closePopupZoom.addEventListener("click",  () => {
+  ClosePopup(popupZoom);
 });
 
-
+//Массив
 const initialCards = [
   {
     name: "Архыз",
@@ -73,7 +83,7 @@ const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
-
+//Новая карточка
 const cardsContainer = document.querySelector(".elements");
 const template = document.querySelector("#element-template");
 const createCard = (cardInfo) => {
@@ -81,9 +91,12 @@ const createCard = (cardInfo) => {
   const img = newCard.querySelector(".element__image");
   img.src = cardInfo.link;
   img.alt = cardInfo.name;
-
+  img.addEventListener("click", () =>
+    handleOpenZoomPopup(cardInfo.link, cardInfo.name)
+  );
   const titleElement = newCard.querySelector(".element__title");
   titleElement.textContent = cardInfo.name;
+
   const removeBtn = newCard.querySelector(".element__delete");
   const removeHandler = () => {
     newCard.remove();
@@ -94,7 +107,7 @@ const createCard = (cardInfo) => {
   const likeHandler = () => {
     newCard
       .querySelector(".element__like")
-      .classList.toggle(".element__like_active");
+      .classList.toggle("element__like_active");
   };
   likeBtn.addEventListener("click", likeHandler);
   return newCard;
@@ -106,3 +119,26 @@ const renderCard = (cardInfo) => {
 initialCards.forEach((item) => {
   renderCard(item);
 });
+
+//добавление фото
+
+const handleNewPlace = (evt) => {
+  evt.preventDefault();
+  const newPlace = {};
+  newPlace.name = inputPlaceName.value;
+  newPlace.link = inputPlaceLink.value;
+
+  inputPlaceName.value = "";
+  inputPlaceLink.value = "";
+  renderCard(newPlace);
+  ClosePopup(popupNewPlace);
+};
+popupNewPlaceForm.addEventListener("submit", handleNewPlace);
+
+function handleOpenZoomPopup(imgSrc, text) {
+  popupZoom.classList.add("popup_opened");
+  const img = popupZoom.querySelector(".popup__image");
+  img.src = imgSrc;
+  const signatureZoom = popupZoom.querySelector(".popup__image-signature");
+  signatureZoom.textContent = text;
+}
